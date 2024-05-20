24be991af5c9c22d7033b6f4c388b016e1de250a
Cristhian Vanegas <crisvanegas02@gmail.com>
2024-05-20 17:58:40 -0500

    Se agrega input inicio

--- angular/src/app/components/contador-h/contador-h.component.ts
+++ angular/src/app/components/contador-h/contador-h.component.ts
@@ -1,26 +1,35 @@
-import { Component,Output,EventEmitter} from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contador-h',
   standalone: true,
   imports: [],
   templateUrl: './contador-h.component.html',
-  styleUrl: './contador-h.component.css'
+  styleUrl: './contador-h.component.css',
 })
 export class ContadorHComponent {
   contador: number = 0;
 
-  @Output() agregarNumero= new EventEmitter<number>();
- 
-  manejarClickAscendete(){
-    this.contador +=1;
+  @Output() agregarNumeroIncio = new EventEmitter<number>();
+  @Output() agregarNumero = new EventEmitter<number>();
+
+  manejarClickAscendete() {
+    this.contador += 1;
   }
 
-  manejarClickDescendente(){
-    this.contador -=1
+  manejarClickDescendente() {
+    this.contador -= 1;
   }
 
-  enviarNumero(){
+  ManejarClickReset() {
+    this.contador = 0;
+  }
+
+  numeroInicioConteo(numero: string) {
+    this.contador = parseInt(numero);
+  }
+
+  enviarNumero() {
     this.agregarNumero.emit(this.contador);
   }
 }
